5f12f04ed851797f156ad6f239edc879ea0b757d
John <birukovie@gmail.com>
2023-09-14 23:09:15 +0300

    edited user model

--- backend/account/models.py
+++ backend/account/models.py
@@ -3,17 +3,14 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 
 
-class Account(AbstractUser):
+class User(AbstractUser):
     username = models.CharField(
-        'username',
         max_length=50,
         unique=True,
         validators=[RegexValidator(r'^[a-zA-Z0-9_]+$', 'Имя пользователя может содержать символы a-Z, 0-9 и _')],
-        help_text='Required. 50 characters or fewer. Letters, digits and @/./+/-/_ only.'
     )
-    email = models.EmailField(unique=True, blank=False, null=False, help_text='Почта используется')
+    email = models.EmailField(unique=True, blank=False, null=False)
     avatar = models.ImageField(upload_to='user_avatars', blank=True, null=True)
-    following = models.ManyToManyField()
-    friends = models.ManyToManyField()
+    following = models.ManyToManyField('self')
+    friends = models.ManyToManyField('self')
     bio = models.TextField()
-
--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,18 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Account
-
-
-class AccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Account
-        fields = ('avatar', 'following', 'friends', 'bio')
 
 
 class UserSerializer(serializers.ModelSerializer):
-    account = AccountSerializer(required=True)
-
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'account')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'first_name', 'last_name', 'f', 'email', 'password')
