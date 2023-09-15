1c5e4335a378d6115e8a15466b76512a45824f69
John <birukovie@gmail.com>
2023-09-15 21:19:27 +0300

    add post model

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from account.models import User
+from django.contrib.humanize.templatetags import humanize
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_private = models.BooleanField(default=False)
+    likes = models.ManyToManyField(Like, blank=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def get_date(self):
+        return humanize.naturaltime(self.created_at)
