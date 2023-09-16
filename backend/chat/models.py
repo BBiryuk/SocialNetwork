74cc6267b684f05b74928f666e0cad1bfc009680
John <birukovie@gmail.com>
2023-09-16 15:51:27 +0300

    update chat db

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -4,7 +4,7 @@ from django.contrib import humanize
 
 
 class Conversation(models.Model):
-    members = models.ManyToManyField(User, related_name='conversations')
+    members = models.ManyToManyField(User, related_name='conversations', through='Membership')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -15,11 +15,19 @@ class Conversation(models.Model):
         return humanize.naturaltime(self.updated_at)
 
 
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+
+    def get_date_joined(self):
+        return humanize.naturaltime(self.updated_at)
+
+
 class Message(models.Model):
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
